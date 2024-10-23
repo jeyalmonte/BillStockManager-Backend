bcc46c75da178346f3335771289b188af86debf9
Jeyson Almonte <jeysom28@gmail.com>
2024-10-23 00:55:29 -0400

    tests(Products): add category and Product unit tests

---
+++ tests/Domain.UnitTests/Products/CategoryTests.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Products;
+using FluentAssertions;
+using SharedKernel.Results;
+
+namespace Domain.UnitTests.Products;
+public class CategoryTests
+{
+	[Fact]
+	public void Create_ShouldReturnCategoryInstance()
+	{
+		// Act
+		var category = Category.Create(name: "test1", description: "test1");
+
+		// Assert
+		category.Should().NotBeNull();
+		category.Should().BeOfType<Category>();
+	}
+
+	[Fact]
+	public void AddProduct_ShouldReturnConflict_WhenProductAlreadyExists()
+	{
+		// Arrange
+		var category = Category.Create(name: "test1", description: "test1");
+		var product = Product.Create(name: "test1", categoryId: category.Id, description: "test1", price: 10, stock: 10);
+		product.Should().NotBeNull();
+		category.AddProduct(product);
+
+		// Act
+		var result = category.AddProduct(product);
+
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
+	}
+
+	[Fact]
+	public void AddProduct_ShouldAddProduct_WhenProductDoesNotExists()
+	{
+		// Arrange
+		var category = Category.Create(name: "test1", description: "test1");
+		var product = Product.Create(name: "test1", categoryId: category.Id, description: "test1", price: 10, stock: 10);
+		product.Should().NotBeNull();
+
+		// Act
+		var result = category.AddProduct(product);
+
+		// Assert
+		result.HasError.Should().BeFalse();
+		category.Products.Should().Contain(product);
+	}
+
+}
---
+++ tests/Domain.UnitTests/Products/ProductTests.cs
@@ -0,0 +1,124 @@
+﻿using Domain.Products;
+using FluentAssertions;
+using SharedKernel.Results;
+
+namespace Domain.UnitTests.Products;
+public class ProductTests
+{
+	[Fact]
+	public void Create_ShouldReturnProductInstance()
+	{
+		// Act
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
+
+		// Assert
+		product.Should().NotBeNull();
+		product.Should().BeOfType<Product>();
+	}
+
+	[Fact]
+	public void ReduceStock_ShouldReturnConflict_WhenQuantityIsLessThanOrEqualToZero()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
+
+		// Act
+		var result = product.ReduceStock(quantity: 0);
+
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
+	}
+
+	[Fact]
+	public void ReduceStock_ShouldReturnConflict_WhenQuantityExceedsAvailableStock()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
+
+		// Act
+		var result = product.ReduceStock(quantity: 20);
+
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
+	}
+
+	[Fact]
+	public void ReduceStock_ShouldReturnSuccess()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
+
+		// Act
+		var result = product.ReduceStock(quantity: 5);
+
+		// Assert
+		result.HasError.Should().BeFalse();
+		product.Stock.Should().Be(5);
+	}
+
+	[Fact]
+	public void IncreaseStock_ShouldReturnConflict_WhenQuantityIsLessThanOrEqualToZero()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
+
+		// Act
+		var result = product.IncreaseStock(quantity: 0);
+
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
+	}
+
+	[Fact]
+	public void IncreaseStock_ShouldReturnSuccess()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
+
+		// Act
+		var result = product.IncreaseStock(quantity: 5);
+
+		// Assert
+		result.HasError.Should().BeFalse();
+		product.Stock.Should().Be(15);
+	}
+}
