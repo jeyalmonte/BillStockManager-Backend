01ab701f5dc025ef90980d3025f98cd66095e49d
Jeyson Almonte <jeysom28@gmail.com>
2024-10-24 15:21:06 -0400

    feat(Infrastructure): add entities configuration

---
+++ src/Infrastructure/Customers/Persistence/CustomerConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Customers.Persistence;
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+	public void Configure(EntityTypeBuilder<Customer> builder)
+	{
+		builder.ToTable("Customers");
+
+		builder.HasKey(c => c.Id);
+
+		builder.Property(c => c.Id)
+			.ValueGeneratedNever();
+
+		builder.Property(c => c.FullName)
+			.IsRequired();
+
+		builder.Property(c => c.Nickname);
+
+		builder.Property(c => c.Gender)
+		   .HasConversion(
+			   v => v.ToString(),
+			   v => Enum.Parse<GenderType>(v))
+		   .IsRequired();
+
+		builder.Property(c => c.Email);
+
+		builder.Property(c => c.PhoneNumber);
+
+		builder.Property(c => c.Address);
+
+		builder
+			.HasMany(c => c.Invoices)
+			.WithOne(c => c.Customer)
+			.HasForeignKey(i => i.CustomerId)
+			.OnDelete(DeleteBehavior.Restrict);
+
+		builder.HasQueryFilter(c => !c.IsDeleted);
+	}
+}
---
+++ src/Infrastructure/Products/Persistence/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Products;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Products.Persistence.Configurations;
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+	public void Configure(EntityTypeBuilder<Category> builder)
+	{
+		builder.ToTable("Categories");
+
+		builder.HasKey(c => c.Id);
+
+		builder.Property(c => c.Id)
+			.ValueGeneratedNever();
+
+		builder.Property(c => c.Name);
+
+		builder.Property(c => c.Description);
+
+		builder.HasQueryFilter(c => !c.IsDeleted);
+	}
+}
