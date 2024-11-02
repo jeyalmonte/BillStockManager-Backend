86deb439f6993d522906dfb72ea36e0effe4f7b7
Jeyson Almonte <jeysom28@gmail.com>
2024-11-02 17:35:23 -0400

    feat(AppDbInitializer): add SeedRolesasync method

--- src/Infrastructure/Common/Persistence/AppDbInitializer.cs
+++ src/Infrastructure/Common/Persistence/AppDbInitializer.cs
@@ -9,50 +9,63 @@ namespace Infrastructure.Common.Persistence;
 
 public interface IAppDbInitializer
 {
-    Task InitializeAsync();
-    Task SeedAsync();
+	Task InitializeAsync();
+	Task SeedAsync();
 }
 
 internal class AppDbInitializer(
-    AppDbContext db,
-    UserManager<User> userManager,
-    RoleManager<IdentityRole> roleManager) : IAppDbInitializer
+	AppDbContext db,
+	UserManager<User> userManager,
+	RoleManager<IdentityRole> roleManager) : IAppDbInitializer
 {
-    private const string AdministratorRoleName = "Administrator";
+	private const string AdministratorRoleName = "Administrator";
 
-    public async Task InitializeAsync()
-    {
-        await db.Database.MigrateAsync();
-    }
+	public async Task InitializeAsync()
+	{
+		await db.Database.MigrateAsync();
+	}
 
-    public async Task SeedAsync()
-    {
-        await SeedAdministratorAsync();
+	public async Task SeedAsync()
+	{
+		await SeedRolesAsync();
+		await SeedAdministratorAsync();
 
-        var categories = CategoryData.GetData();
+		var categories = CategoryData.GetData();
 
-        if (!db.Set<Category>().Any())
-        {
-            db.AddRange(categories);
-        }
-        if (!db.Set<Product>().Any())
-            db.AddRange(ProductData.GetData(categories));
-        if (!db.Set<Customer>().Any())
-            db.AddRange(CustomerData.GetData());
+		if (!db.Set<Category>().Any())
+		{
+			db.AddRange(categories);
+		}
+		if (!db.Set<Product>().Any())
+			db.AddRange(ProductData.GetData(categories));
+		if (!db.Set<Customer>().Any())
+			db.AddRange(CustomerData.GetData());
 
-        await db.SaveChangesAsync(CancellationToken.None);
-    }
+		await db.SaveChangesAsync(CancellationToken.None);
+	}
 
-    private async Task SeedAdministratorAsync()
-    {
-        if (await roleManager.RoleExistsAsync(AdministratorRoleName))
-            return;
 
-        var adminRole = new IdentityRole(AdministratorRoleName);
-        await roleManager.CreateAsync(adminRole);
+	private async Task SeedRolesAsync()
+	{
+		var roles = new[]
+		{
+			new IdentityRole("User"),
+			new IdentityRole("Manager"),
+			new IdentityRole("Administrator")
+		};
 
-        var adminUser = new User("Admin", "admin", "admin@admin.com");
-        await userManager.CreateAsync(adminUser, "admin1234");
-        await userManager.AddToRoleAsync(adminUser, AdministratorRoleName);
-    }
+		foreach (var role in roles)
+		{
+			if (await roleManager.RoleExistsAsync(role.Name!))
+				continue;
+
+			await roleManager.CreateAsync(role);
+		}
+	}
+	private async Task SeedAdministratorAsync()
+	{
+		var adminUser = new User("Admin", "admin", "admin@admin.com");
+		await userManager.CreateAsync(adminUser, "admin1234");
+		await userManager.AddToRoleAsync(adminUser, AdministratorRoleName);
+	}
 }
