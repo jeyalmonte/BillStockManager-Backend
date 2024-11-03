1d5421c870df7f766adc31c00d3d7db655a28084
Jeyson Almonte <jeysom@live.com>
2024-11-03 02:07:05 -0400

    feat(customers) : add CustomerCreatedEventHandler

---
+++ src/Application/Customers/Events/CustomerCreatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Customers.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Customers.Events;
+public class CustomerCreatedEventHandler(ILogger<CustomerCreatedEventHandler> logger)
+    : INotificationHandler<CustomerCreatedEvent>
+{
+    public async Task Handle(CustomerCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        await Task.CompletedTask;
+
+        logger.LogInformation("Customer with id '{Id} was created.'", notification.Customer.Id);
+    }
+}
