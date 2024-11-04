804d61e07485cbf48ecbe22d4ddcb5fef2e8f72f
Jeyson Almonte <jeysom@live.com>
2024-11-04 00:28:10 -0400

    tests: add RegisterUserCommandTests

---
+++ tests/Application.UnitTests/Identity/Commands/RegisterUserCommandTests.cs
@@ -0,0 +1,70 @@
+﻿using Application.Identity.Commands.Register;
+using Application.Identity.Interfaces;
+using Application.Identity.Models;
+
+namespace Application.UnitTests.Identity.Commands;
+public class RegisterUserCommandTests
+{
+    private readonly Mock<IIdentityService> _identityService = new();
+    private readonly CancellationToken _cancellationToken = new();
+
+    [Fact]
+    public void RegisterUserCommand_ShouldReturnValidationError_WhenUsernameIsEmpty()
+    {
+        // Arrange
+        var command = CreateRegisterUserCommand(includeUsername: false);
+        var validator = new RegisterUserCommandValidator();
+
+        // Act
+        var result = validator.Validate(command);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task RegisterUserCommand_ShouldReturnError_WhenRegisterFails()
+    {
+        // Arrange 
+        var command = CreateRegisterUserCommand();
+        var response = (Result<Success>)Error.Conflict();
+
+        _identityService.Setup(x => x.Register(It.IsAny<UserRegisterRequest>()))
+            .ReturnsAsync(response);
+
+        var handler = new RegisterUserCommandHandler(_identityService.Object);
+
+        // Act
+        var result = await handler.Handle(command, _cancellationToken);
+
+        //Assert
+        result.HasError.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task RegisterUserCommand_ShouldRegisterUser()
+    {
+        // Arrange 
+        var command = CreateRegisterUserCommand();
+        var response = (Result<Success>)Result.Success;
+
+        _identityService.Setup(x => x.Register(It.IsAny<UserRegisterRequest>()))
+            .ReturnsAsync(response);
+
+        var handler = new RegisterUserCommandHandler(_identityService.Object);
+
+        // Act
+        var result = await handler.Handle(command, _cancellationToken);
+
+        //Assert
+        result.HasError.Should().BeFalse();
+    }
+
+    private static RegisterUserCommand CreateRegisterUserCommand(bool includeUsername = true)
+       => new(
+           Name: "test",
+           Username: includeUsername ? "test" : default!,
+           Email: "tes",
+           Password: "test");
+}
+
