a2a6526ded7e1d37e2ed1131966f2e21c359da8b
Jeyson Almonte <jeysom28@gmail.com>
2024-11-12 16:47:25 -0400

    feat(inventory): implement GetAllCategories
    - Added GetAllCategories query and query handler
    - Added GetAll endpoint in CategoriesController

--- src/Api/Controllers/CategoriesController.cs
+++ src/Api/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Inventory.Categories.Commands.Create;
+using Application.Inventory.Categories.Queries.GetAll;
 using Microsoft.AspNetCore.Mvc;
 using SharedKernel.Contracts.Categories;
 using SharedKernel.Results;
@@ -7,17 +8,29 @@ namespace Api.Controllers;
 [Route("api/inventory/[controller]")]
 public class CategoriesController : ApiController
 {
-    [HttpPost]
-    [EndpointSummary("Create a category")]
-    [ProducesResponseType(typeof(CategoryResponse), StatusCodes.Status201Created)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status409Conflict)]
-    public async Task<IActionResult> Create([FromBody] CreateCategoryCommand command)
-    {
-        var result = await Sender.Send(command);
+	[HttpPost]
+	[EndpointSummary("Create a category")]
+	[ProducesResponseType(typeof(CategoryResponse), StatusCodes.Status201Created)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status409Conflict)]
+	public async Task<IActionResult> Create([FromBody] CreateCategoryCommand command)
+	{
+		var result = await Sender.Send(command);
 
-        return result.Match(
-            category => Created("", category),
-            Problem);
-    }
+		return result.Match(
+			category => Created("", category),
+			Problem);
+	}
+
+	[HttpGet]
+	[EndpointSummary("Get all categories")]
+	[ProducesResponseType(typeof(ICollection<CategoryResponse>), StatusCodes.Status200OK)]
+	public async Task<IActionResult> GetAll()
+	{
+		var result = await Sender.Send(new GetAllCategoriesQuery());
+
+		return result.Match(
+			Ok,
+			Problem);
+	}
 }
