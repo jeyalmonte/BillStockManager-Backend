07878fd680431ce3e8f874e92d77e6eff79ae5fc
Jeyson Almonte <jeysom@live.com>
2024-11-11 23:21:00 -0400

    feat(inventory): implement CategoryRepository
    - Add methods to handle category retrieval and storage

---
+++ src/Domain/Inventory/Repositories/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using SharedKernel.Interfaces;
+
+namespace Domain.Inventory.Repositories;
+public interface ICategoryRepository : IRepository<Category>
+{
+    Task<Category?> GetByNameAsync(string name, CancellationToken cancellationToken = default);
+    Task<ICollection<Category>> GetAllAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Infrastructure/Inventory/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Inventory;
+using Domain.Inventory.Repositories;
+using Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Inventory.Persistence.Repositories;
+internal class CategoryRepository(AppDbContext dbContext)
+    : AppDbContextAccess<Category>(dbContext), ICategoryRepository
+{
+    public void Add(Category entity) => Entities.Add(entity);
+    public async Task<ICollection<Category>> GetAllAsync(CancellationToken cancellationToken = default)
+        => await EntitiesAsNoTracking.ToListAsync(cancellationToken);
+    public async Task<Category?> GetByIdAsync(Guid id, bool asNoTracking = true, CancellationToken cancellationToken = default)
+        => asNoTracking
+        ? await EntitiesAsNoTracking.SingleOrDefaultAsync(x => x.Id == id, cancellationToken)
+        : await Entities.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+    public Task<Category?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+        => EntitiesAsNoTracking.SingleOrDefaultAsync(x => x.Name == name, cancellationToken);
+}
