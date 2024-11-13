bcaad9c3d8935a584b473282e04b927a41e671a9
Jeyson Almonte <jeysom28@gmail.com>
2024-11-13 10:00:18 -0400

    fix(domain):  change ProductCreatedDomainEvent to  sealed record to pass architecture tests

--- src/Domain/Inventory/Events/ProductCreatedDomainEvent.cs
+++ src/Domain/Inventory/Events/ProductCreatedDomainEvent.cs
@@ -1,4 +1,4 @@
 ﻿using SharedKernel.Domain;
 
 namespace Domain.Inventory.Events;
-public record ProductCreatedDomainEvent(Guid Id) : IDomainEvent;
+public sealed record ProductCreatedDomainEvent(Guid Id) : IDomainEvent;
