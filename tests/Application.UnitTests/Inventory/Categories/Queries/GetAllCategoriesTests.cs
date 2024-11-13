63aad54188aba1437c898edb9a8de6687c42de67
Jeyson Almonte <jeysom@live.com>
2024-11-12 22:57:34 -0400

    test: add GetAllCategoriesTests

---
+++ tests/Application.UnitTests/Inventory/Categories/Queries/GetAllCategoriesTests.cs
@@ -0,0 +1,27 @@
+﻿using Application.Inventory.Categories.Queries.GetAll;
+using Domain.Inventory;
+using Domain.Inventory.Repositories;
+using SharedKernel.Contracts.Categories;
+
+namespace Application.UnitTests.Inventory.Categories.Queries;
+public class GetAllCategoriesTests
+{
+    private readonly Mock<ICategoryRepository> _categoryRepository = new();
+
+    [Fact]
+    public async Task GetAllCategories_ShouldReturnListOfCategoryResponse()
+    {
+        // Arrange
+        _categoryRepository.Setup(x => x.GetAllAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<Category>());
+
+        var handler = new GetAllCategoriesQueryHandler(_categoryRepository.Object);
+
+        // Act
+        var result = await handler.Handle(new GetAllCategoriesQuery(), CancellationToken.None);
+
+        //Assert
+        result.Value.Should().NotBeNull();
+        result.Value.Should().BeOfType<List<CategoryResponse>>();
+    }
+}
