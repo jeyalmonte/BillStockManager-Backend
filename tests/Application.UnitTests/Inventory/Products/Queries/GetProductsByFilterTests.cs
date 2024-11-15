0531bea4e6d46fd2a7037d61fdaa20c618b970e4
Jeyson Almonte <jeysom@live.com>
2024-11-14 23:59:29 -0400

    test: add GetProductsByFilterTests

---
+++ tests/Application.UnitTests/Inventory/Products/Queries/GetProductsByFilterTests.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common.Models;
+using Application.Inventory.Products.Queries.GetByFilter;
+using Domain.Inventory;
+using Domain.Inventory.Repositories;
+using SharedKernel.Contracts.Products;
+using SharedKernel.Specification;
+
+namespace Application.UnitTests.Inventory.Products.Queries;
+public class GetProductsByFilterTests
+{
+    private readonly Mock<IProductRepository> _productRepository = new();
+
+    [Fact]
+    public async Task GetProductsByFilter_ShouldReturnPaginatedResultOfCustomerResponse()
+    {
+        // Arrange
+        var query = new GetProductsByFilterQuery(
+            Name: "test",
+            SortBy: "test",
+            OrderBy: SharedKernel.Enums.OrderType.Descending);
+
+        _productRepository.Setup(x => x.GetAllBySpecAsync(It.IsAny<Specification<Product>>(), It.IsAny<int>(), It.IsAny<int>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync([]);
+        var handler = new GetProductsByFilterQueryHandler(_productRepository.Object);
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.HasError.Should().BeFalse();
+        result.Value.Should().BeOfType<PaginatedResult<ProductResponse>>();
+    }
+}
