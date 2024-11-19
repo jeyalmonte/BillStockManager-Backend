79fa6c079fa811ac0d67cb97e2a1e43bd614d70c
Jeyson Almonte <jeysom28@gmail.com>
2024-11-19 17:14:47 -0400

    test(architecture):  add event handler tests

--- tests/Architecture.Tests/Application/ApplicationTests.cs
+++ tests/Architecture.Tests/Application/ApplicationTests.cs
@@ -4,72 +4,98 @@ using SharedKernel.Interfaces.Messaging;
 namespace Architecture.Tests.Application;
 public class ApplicationTests : BaseTests
 {
-    [Fact]
-    public void Commands_ShouldHaveNameEndingWithCommand()
-    {
-        var result = Types.InAssembly(ApplicationAssembly)
-            .That()
-            .ImplementInterface(typeof(ICommand))
-            .Or()
-            .ImplementInterface(typeof(ICommand<>))
-            .Should()
-            .HaveNameEndingWith("Command")
-            .GetResult();
+	[Fact]
+	public void Commands_ShouldHaveNameEndingWithCommand()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.ImplementInterface(typeof(ICommand))
+			.Or()
+			.ImplementInterface(typeof(ICommand<>))
+			.Should()
+			.HaveNameEndingWith("Command")
+			.GetResult();
 
-        result.IsSuccessful.Should().BeTrue();
-    }
+		result.IsSuccessful.Should().BeTrue();
+	}
 
-    [Fact]
-    public void CommandHandlers_ShouldHaveNameEndingWithCommandHandler()
-    {
-        var result = Types.InAssembly(ApplicationAssembly)
-            .That()
-            .ImplementInterface(typeof(ICommandHandler<>))
-            .Or()
-            .ImplementInterface(typeof(ICommandHandler<,>))
-            .Should()
-            .HaveNameEndingWith("CommandHandler")
-            .GetResult();
+	[Fact]
+	public void CommandHandlers_ShouldHaveNameEndingWithCommandHandler()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.ImplementInterface(typeof(ICommandHandler<>))
+			.Or()
+			.ImplementInterface(typeof(ICommandHandler<,>))
+			.Should()
+			.HaveNameEndingWith("CommandHandler")
+			.GetResult();
 
-        result.IsSuccessful.Should().BeTrue();
-    }
+		result.IsSuccessful.Should().BeTrue();
+	}
 
-    [Fact]
-    public void Queries_ShouldHaveNameEndingWithQuery()
-    {
-        var result = Types.InAssembly(ApplicationAssembly)
-            .That()
-            .Inherit(typeof(IQuery<>))
-            .Should()
-            .HaveNameEndingWith("Query")
-            .GetResult();
+	[Fact]
+	public void Queries_ShouldHaveNameEndingWithQuery()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.Inherit(typeof(IQuery<>))
+			.Should()
+			.HaveNameEndingWith("Query")
+			.GetResult();
 
-        result.IsSuccessful.Should().BeTrue();
-    }
+		result.IsSuccessful.Should().BeTrue();
+	}
 
-    [Fact]
-    public void QueryHandlers_ShouldHaveNameEndingWithQueryHandler()
-    {
-        var result = Types.InAssembly(ApplicationAssembly)
-            .That()
-            .ImplementInterface(typeof(IQueryHandler<,>))
-            .Should()
-            .HaveNameEndingWith("QueryHandler")
-            .GetResult();
+	[Fact]
+	public void QueryHandlers_ShouldHaveNameEndingWithQueryHandler()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.ImplementInterface(typeof(IQueryHandler<,>))
+			.Should()
+			.HaveNameEndingWith("QueryHandler")
+			.GetResult();
 
-        result.IsSuccessful.Should().BeTrue();
-    }
+		result.IsSuccessful.Should().BeTrue();
+	}
 
-    [Fact]
-    public void Validators_ShouldHaveNameEndingWithValidator()
-    {
-        var result = Types.InAssembly(ApplicationAssembly)
-            .That()
-            .Inherit(typeof(AbstractValidator<>))
-            .Should()
-            .HaveNameEndingWith("Validator")
-            .GetResult();
+	[Fact]
+	public void Validators_ShouldHaveNameEndingWithValidator()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.Inherit(typeof(AbstractValidator<>))
+			.Should()
+			.HaveNameEndingWith("Validator")
+			.GetResult();
 
-        result.IsSuccessful.Should().BeTrue();
-    }
+		result.IsSuccessful.Should().BeTrue();
+	}
+
+	[Fact]
+	public void EventHandler_ShouldHaveNameEndingWithEventHandler()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.ImplementInterface(typeof(IEventHandler<>))
+			.Should()
+			.HaveNameEndingWith("EventHandler")
+			.GetResult();
+
+		result.IsSuccessful.Should().BeTrue();
+	}
+
+	[Fact]
+	public void EventHandlers_ShouldNotBePublic()
+	{
+		var result = Types.InAssembly(ApplicationAssembly)
+			.That()
+			.ImplementInterface(typeof(IEventHandler<>))
+			.Should()
+			.NotBePublic()
+			.GetResult();
+
+		result.IsSuccessful.Should().BeTrue();
+	}
 }
