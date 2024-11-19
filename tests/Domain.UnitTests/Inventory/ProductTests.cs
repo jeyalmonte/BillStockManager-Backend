8841858fe92be124caafaddc010ce69c16d4194d
Jeyson Almonte <jeysom28@gmail.com>
2024-11-19 00:13:05 -0400

    fix(inventory): correct Product domain tests

--- tests/Domain.UnitTests/Inventory/ProductTests.cs
+++ tests/Domain.UnitTests/Inventory/ProductTests.cs
@@ -5,164 +5,164 @@ using SharedKernel.Results;
 namespace Domain.UnitTests.Inventory;
 public class ProductTests
 {
-    [Fact]
-    public void Create_ShouldReturnProductInstance()
-    {
-        // Act
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void Create_ShouldReturnProductInstance()
+	{
+		// Act
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Assert
-        product.Should().NotBeNull();
-        product.Should().BeOfType<Product>();
-    }
+		// Assert
+		product.Should().NotBeNull();
+		product.Should().BeOfType<Product>();
+	}
 
-    [Fact]
-    public void Update_ShouldUpdateProduct()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void Update_ShouldUpdateProduct()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        var nameUpdated = "TestUpdate";
+		var nameUpdated = "TestUpdate";
 
-        // Act
-        product.Update(
-            name: nameUpdated,
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100);
+		// Act
+		product.Update(
+			name: nameUpdated,
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100);
 
-        // Assert
-        product.Name.Should().Be(nameUpdated);
-    }
+		// Assert
+		product.Name.Should().Be(nameUpdated);
+	}
 
-    [Fact]
-    public void Remove_ShouldMarkProductAsDeleted()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void Remove_ShouldMarkProductAsDeleted()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Act
-        product.Remove();
+		// Act
+		product.Remove();
 
-        // Assert
-        product.IsDeleted.Should().BeTrue();
-    }
+		// Assert
+		product.IsDeleted.Should().BeTrue();
+	}
 
-    [Fact]
-    public void ReduceStock_WhenQuantityIsLessThanOrEqualToZero_ShouldReturnConflict()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void ReduceStock_WhenQuantityIsLessThanOrEqualToZero_ShouldReturnFailure()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Act
-        var result = product.ReduceStock(quantity: 0);
+		// Act
+		var result = product.ReduceStock(quantity: 0);
 
-        // Assert
-        result.HasError.Should().BeTrue();
-        result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
-    }
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Failure);
+	}
 
-    [Fact]
-    public void ReduceStock_WhenQuantityExceedsAvailableStock_ShouldReturnConflict()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void ReduceStock_WhenQuantityExceedsAvailableStock_ShouldReturnConflict()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Act
-        var result = product.ReduceStock(quantity: 20);
+		// Act
+		var result = product.ReduceStock(quantity: 20);
 
-        // Assert
-        result.HasError.Should().BeTrue();
-        result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
-    }
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
+	}
 
-    [Fact]
-    public void ReduceStock_ShouldReduceStock()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void ReduceStock_ShouldReduceStock()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Act
-        var result = product.ReduceStock(quantity: 5);
+		// Act
+		var result = product.ReduceStock(quantity: 5);
 
-        // Assert
-        result.HasError.Should().BeFalse();
-        product.Stock.Should().Be(5);
-    }
+		// Assert
+		result.HasError.Should().BeFalse();
+		product.Stock.Should().Be(5);
+	}
 
-    [Fact]
-    public void IncreaseStock_WhenQuantityIsLessThanOrEqualToZero_ShouldReturnConflict()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void IncreaseStock_WhenQuantityIsLessThanOrEqualToZero_ShouldReturnConflict()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Act
-        var result = product.IncreaseStock(quantity: 0);
+		// Act
+		var result = product.IncreaseStock(quantity: 0);
 
-        // Assert
-        result.HasError.Should().BeTrue();
-        result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
-    }
+		// Assert
+		result.HasError.Should().BeTrue();
+		result.Errors.First().ErrorType.Should().Be(ErrorType.Conflict);
+	}
 
-    [Fact]
-    public void IncreaseStock_ShoulIncreaseStock()
-    {
-        // Arrange
-        var product = Product.Create(
-            name: "Test",
-            categoryId: Guid.NewGuid(),
-            description: null,
-            price: 100,
-            stock: 10
-        );
+	[Fact]
+	public void IncreaseStock_ShoulIncreaseStock()
+	{
+		// Arrange
+		var product = Product.Create(
+			name: "Test",
+			categoryId: Guid.NewGuid(),
+			description: null,
+			price: 100,
+			stock: 10
+		);
 
-        // Act
-        var result = product.IncreaseStock(quantity: 5);
+		// Act
+		var result = product.IncreaseStock(quantity: 5);
 
-        // Assert
-        result.HasError.Should().BeFalse();
-        product.Stock.Should().Be(15);
-    }
+		// Assert
+		result.HasError.Should().BeFalse();
+		product.Stock.Should().Be(15);
+	}
 }
