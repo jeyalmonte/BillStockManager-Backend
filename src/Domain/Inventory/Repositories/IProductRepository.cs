38e57fb2383b19d22f7d6ae1fe16223e1291a11c
Jeyson Almonte <jeysom28@gmail.com>
2024-11-24 02:18:53 -0400

    feat(products): add GetByIdsAsync method to ProductRepository

--- src/Domain/Inventory/Repositories/IProductRepository.cs
+++ src/Domain/Inventory/Repositories/IProductRepository.cs
@@ -4,14 +4,17 @@ using SharedKernel.Specification;
 namespace Domain.Inventory.Repositories;
 public interface IProductRepository : IRepository<Product>
 {
-    Task<Product?> GetByNameAsync(string name,
-        CancellationToken cancellationToken = default);
+	Task<Product?> GetByNameAsync(string name,
+		CancellationToken cancellationToken = default);
 
-    Task<ICollection<Product>> GetAllBySpecAsync(Specification<Product> specification,
-        int pageSize = 0,
-        int pageNumber = 0,
-        CancellationToken cancellationToken = default);
+	Task<ICollection<Product>> GetAllBySpecAsync(Specification<Product> specification,
+		int pageSize = 0,
+		int pageNumber = 0,
+		CancellationToken cancellationToken = default);
 
-    Task<int> GetTotalAsync(Specification<Product> specification,
-        CancellationToken cancellationToken = default);
+	Task<int> GetTotalAsync(Specification<Product> specification,
+		CancellationToken cancellationToken = default);
+
+	Task<ICollection<Product>> GetByIdsAsync(List<Guid> ids,
+		CancellationToken cancellationToken = default);
 }
--- src/Infrastructure/Inventory/Persistence/Repositories/ProductRepository.cs
+++ src/Infrastructure/Inventory/Persistence/Repositories/ProductRepository.cs
@@ -7,28 +7,31 @@ using SharedKernel.Specification;
 
 namespace Infrastructure.Inventory.Persistence.Repositories;
 internal class ProductRepository(AppDbContext dbContext)
-    : AppDbContextAccess<Product>(dbContext), IProductRepository
+	: AppDbContextAccess<Product>(dbContext), IProductRepository
 {
-    public void Add(Product entity) => Entities.Add(entity);
+	public void Add(Product entity) => Entities.Add(entity);
 
-    public Task<Product?> GetByIdAsync(Guid id, bool asNoTracking = true, CancellationToken cancellationToken = default)
-        => asNoTracking
-        ? EntitiesAsNoTracking.SingleOrDefaultAsync(x => x.Id == id, cancellationToken)
-        : Entities.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
+	public Task<Product?> GetByIdAsync(Guid id, bool asNoTracking = true, CancellationToken cancellationToken = default)
+		=> asNoTracking
+		? EntitiesAsNoTracking.SingleOrDefaultAsync(x => x.Id == id, cancellationToken)
+		: Entities.SingleOrDefaultAsync(x => x.Id == id, cancellationToken);
 
-    public Task<Product?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
-        => EntitiesAsNoTracking.SingleOrDefaultAsync(x => x.Name == name, cancellationToken);
+	public Task<Product?> GetByNameAsync(string name, CancellationToken cancellationToken = default)
+		=> EntitiesAsNoTracking.SingleOrDefaultAsync(x => x.Name == name, cancellationToken);
 
-    public async Task<ICollection<Product>> GetAllBySpecAsync(Specification<Product> specification,
-        int pageSize = 0,
-        int pageNumber = 0,
-        CancellationToken cancellationToken = default)
-        => await EntitiesAsNoTracking
-            .Specify(specification)
-            .ToPaginatedListAsync(pageNumber, pageSize, cancellationToken);
+	public async Task<ICollection<Product>> GetAllBySpecAsync(Specification<Product> specification,
+		int pageSize = 0,
+		int pageNumber = 0,
+		CancellationToken cancellationToken = default)
+		=> await EntitiesAsNoTracking
+			.Specify(specification)
+			.ToPaginatedListAsync(pageNumber, pageSize, cancellationToken);
 
-    public Task<int> GetTotalAsync(Specification<Product> specification, CancellationToken cancellationToken = default)
-        => EntitiesAsNoTracking
-            .Specify(specification)
-            .CountAsync(cancellationToken);
+	public Task<int> GetTotalAsync(Specification<Product> specification, CancellationToken cancellationToken = default)
+		=> EntitiesAsNoTracking
+			.Specify(specification)
+			.CountAsync(cancellationToken);
+
+	public async Task<ICollection<Product>> GetByIdsAsync(List<Guid> ids, CancellationToken cancellationToken = default)
+		=> await EntitiesAsNoTracking.Where(x => ids.Contains(x.Id)).ToListAsync(cancellationToken);
 }
