7894d225cdef7eb0404a7385d2338321cef0fff1
Jeyson Almonte <jeysom28@gmail.com>
2025-05-10 18:01:45 -0400

    refactor: change PaginatedQuery to be an abstract record

--- src/SharedKernel/Pagination/PaginatedQuery.cs
+++ src/SharedKernel/Pagination/PaginatedQuery.cs
@@ -1,21 +1,20 @@
 ﻿namespace SharedKernel.Pagination;
 
-public record PaginatedQuery
+public abstract record PaginatedQuery
 {
-    private const int DefaultPageNumber = 1;
-    private const int DefaultPageSize = 20;
-    private readonly int? _pageSize;
-    private readonly int? _pageNumber;
+	private const int DefaultPageNumber = 1;
+	private const int DefaultPageSize = 20;
+	private readonly int? _pageSize;
+	private readonly int? _pageNumber;
 
-    public int PageNumber
-    {
-        get => _pageNumber ?? DefaultPageNumber;
-        init => _pageNumber = value;
-    }
-    public int PageSize
-    {
-        get => _pageSize ?? DefaultPageSize;
-        init => _pageSize = value;
-    }
-
+	public int PageNumber
+	{
+		get => _pageNumber ?? DefaultPageNumber;
+		init => _pageNumber = value;
+	}
+	public int PageSize
+	{
+		get => _pageSize ?? DefaultPageSize;
+		init => _pageSize = value;
+	}
 }
